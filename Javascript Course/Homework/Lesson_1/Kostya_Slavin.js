893ca4cf7de1983868e07af4f3a0d22f7eedf269
Konstantin Slavin <skpalad@gmail.com>
2017-02-25 00:06:01 +0200

    Update Kostya_Slavin.js

--- Javascript Course/Homework/Lesson_1/Kostya_Slavin.js
+++ Javascript Course/Homework/Lesson_1/Kostya_Slavin.js
@@ -1,6 +1,6 @@
 function getCentury(){
 	var year = prompt('Введите год от 1 до 2017');
 	return (!isNaN(parseFloat(year)) && isFinite(year)) ? 
-		( year < 1 || year > 2017) ? 'Год указан не правильно' : 
+		( year < 1 || year > 2017) ? 'Год указан неправильно' : 
 			Math.ceil(year / 100) : 'Неверный формат данных';
 }
