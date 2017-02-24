213b961aeb728af76f8c2bffa59786775ba30848
Konstantin Slavin <skpalad@gmail.com>
2017-02-24 23:54:30 +0200

    add file
    add homework to the lesson1

---
+++ Javascript Course/Homework/Lesson_1/Kostya_Slavin.js
@@ -0,0 +1,6 @@
+function getCentury(){
+	var year = prompt('Введите год от 1 до 2017');
+	return (!isNaN(parseFloat(year)) && isFinite(year)) ? 
+		( year < 1 || year > 2017) ? 'Год указан не правильно' : 
+			Math.ceil(year / 100) : 'Неверный формат данных';
+}
