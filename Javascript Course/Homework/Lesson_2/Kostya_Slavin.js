23cf638ea515c801a9df9e7265016e440435e1a8
Konstantin Slavin <skpalad@gmail.com>
2017-03-01 12:25:03 +0200

    Update Kostya_Slavin.js

--- Javascript Course/Homework/Lesson_2/Kostya_Slavin.js
+++ Javascript Course/Homework/Lesson_2/Kostya_Slavin.js
@@ -1,14 +1,13 @@
 function getMaxMul(arr) {
-	var len = arr.length,
-			res = 0,
-			max = 0;
+  var len = arr.length,
+      res = 0,
+      max = 0;
 
-		for (var i = 0; i < len; i++) {
-			res = arr[i] * arr[i+1];
-			max = (max < res) ? res : max;
-		}
-	return max;
+  for (var i = 0; i < len; i++) {
+    res = arr[i] * arr[i+1];
+    max = (max < res) ? res : max;
+  }
+  return max;
 }
-
 
 var arr = [30, 6, -8, -50, 7, 3];
