c9c08d5c7642eb47478d5ebf589d1fc527b76b3a
root <mail@sometext.com>
2017-03-01 12:27:05 +0200

    homework for lesson2

---
+++ Javascript Course/Homework/Lesson_2/Kostya_Slavin.js
@@ -0,0 +1,14 @@
+function getMaxMul(arr) {
+	var len = arr.length,
+			res = 0,
+			max = 0;
+
+		for (var i = 0; i < len; i++) {
+			res = arr[i] * arr[i+1];
+			max = (max < res) ? res : max;
+		}
+	return max;
+}
+
+
+var arr = [30, 6, -8, -50, 7, 3];
