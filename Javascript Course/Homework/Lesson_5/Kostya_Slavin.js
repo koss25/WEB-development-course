bd979ca16682c83d0173c3b474c7cb5fdbb17413
Konstantin Slavin <skpalad@gmail.com>
2017-03-18 18:32:11 +0200

    Update Kostya_Slavin.js
    bug fixed

--- Javascript Course/Homework/Lesson_5/Kostya_Slavin.js
+++ Javascript Course/Homework/Lesson_5/Kostya_Slavin.js
@@ -1,17 +1,19 @@
 function arrayGrow(array) {
-  if (array.length < 3) return 'add more elements to array';
-  
-  var newAr = [];
-  
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] > array[i+1]) {
-      newAr.push(array[i]);
+  if (array.length < 3) return 'add more elements to array';  
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > array[i+1]) {      
+        array.splice(i,1);
+          for (var j = 0; j < array.length; j++) {
+            if (array[j] >= array[j+1]) {
+              return false;
+            }
+          }
+       }
     }
-  }
- return newAr.length == 1;    
+ return true;
 }
 
-console.log(arrayGrow([1,2,1,2])); // true
+console.log(arrayGrow([1,2,1,2])); // false
 console.log(arrayGrow([-1,-2,3])); // true
 console.log(arrayGrow([1,-2,5,56,46])); // false
 console.log(arrayGrow([-3,16])); // add more elements to array
