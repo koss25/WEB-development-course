cce437841d85477737e7aac0b251a58830e2982d
Konstantin Slavin <skpalad@gmail.com>
2017-03-18 18:49:59 +0200

    Update Kostya_Slavin.js
    bug in array ~ 1,1,2,2 - fixed

--- Javascript Course/Homework/Lesson_5/Kostya_Slavin.js
+++ Javascript Course/Homework/Lesson_5/Kostya_Slavin.js
@@ -1,7 +1,7 @@
 function arrayGrow(array) {
   if (array.length < 3) return 'add more elements to array';  
     for (var i = 0; i < array.length; i++) {
-      if (array[i] > array[i+1]) {      
+      if (array[i] >= array[i+1]) {      
         array.splice(i,1);
           for (var j = 0; j < array.length; j++) {
             if (array[j] >= array[j+1]) {
@@ -14,6 +14,7 @@ function arrayGrow(array) {
 }
 
 console.log(arrayGrow([1,2,1,2])); // false
+console.log(arrayGrow([1,1,2,2])); // false
 console.log(arrayGrow([-1,-2,3])); // true
 console.log(arrayGrow([1,-2,5,56,46])); // false
 console.log(arrayGrow([-3,16])); // add more elements to array
