3ea15053a71ea9d71eb4b1c90e2b9a2383f8cd2b
Konstantin Slavin <skpalad@gmail.com>
2017-04-21 23:31:43 +0300

    Update send.php

--- PHP Course/Homework/Lesson_5/Kostya_Slavin/send.php
+++ PHP Course/Homework/Lesson_5/Kostya_Slavin/send.php
@@ -1,114 +1,114 @@
 <?php 
 
 class UserRegister {
-    public $fname;
-    public $lname;
-    public $email;
-    public $type;
-    public $error = false;
+	public $fname;
+	public $lname;
+	public $email;
+	public $type;
+	public $error = false;
+	public $errorAr = [];
+	protected $curDate;
+	protected $filename;    
+	protected $fp;
+	protected $regExp = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/";
+	protected $userRegExp = "/^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/";
+	
+	public function __construct($fn, $ln, $eml, $tp) {
+		$this->fname = $fn;
+		$this->lname = $ln;
+		$this->email = $eml;
+		$this->type = $tp;
+	}
 
-    protected $curDate;
-    protected $file;
-    protected $fp;
-    protected $regExp = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/';
-    protected $fileContent;
+	public function checkData() {
+		if ($this->fname === "" or $this->lname === "" or 
+				$this->email === "" or $this->type === null) {
+				return $this->errorMessages("All fields must be filled and radio button - selected");
+		}
+	}
 
-    public function __construct($fn, $ln, $eml, $tp) {
-        $this->fname = $fn;
-        $this->lname = $ln;
-        $this->email = $eml;
-        $this->type = $tp;
-    }
+	public function checkEmail() {
+		if (!preg_match($this->regExp, $this->email)) {
+				return $this->errorMessages("Email is invalid");
+		}
+	}
 
-    public function checkData() {
-        if ($this->fname === '' or $this->lname === '' or 
-            $this->email === '' or $this->type === '') {
-            $this->error = true;
-            return true;
-        }
-    }
+	public function checkFullName() {
+		if (!preg_match($this->userRegExp, $this->fname) || !preg_match($this->userRegExp, $this->lname)) {
+				return $this->errorMessages("Firstname / Lastname  is invalid");
+		}
+	}
+	
+	public function checkUniq() {
+		if (!file_exists( $this->getFileName() )) {
+				return false;
+		} else {
+			foreach (glob("*.txt") as $filename) {
+				$file = fopen($filename, "r") or die("can't open file");
+					if ($file) {
+						while($buf = fgets($file)) {
+							$dataAr = explode(",", $buf);
+							if ( substr($dataAr[2], 1) === $this->email ) {
+								if ($filename === $this->getFileName()) {
+									return  $this->errorMessages("Email already exist. <br>
+									You registered today.");
+								} else {
+								return  $this->errorMessages("Email already exist. <br>
+								You registered on ".implode("/", explode("_", substr($filename,-14,10))));
+								}
+							}
+						}
+					}
+					fclose($file);
+				}
+			}
+		}
 
-    public function checkEmail() {
-        if (!preg_match($this->regExp, $this->email)) {
-            $this->error = true;
-            return true;
-        }
-    }
+	private function getFileName() {
+		date_default_timezone_set("Europe/Kiev");
+		$this->curDate = date("d_m_Y");
+		$this->filename = "registration_".$this->curDate.".txt";
+		return $this->filename;
+	}
 
-    public function checkNameLength() {
-        if (strlen($this->fname) < 2) {
-            $this->error = true;
-            return true;
-        }
-    }
-    
+	public function addUser() {
+		$this->fp = fopen($this->getFileName(), "a+") or die("can't open file");
+		fwrite($this->fp, $this->fname.", ".$this->lname.", ".$this->email.", ".$this->type."\r\n") or die("can't write data");
+		fclose($this->fp);
+		return "success";
+	}
 
-    public function checkUniq() {        
-        if (!file_exists( $this->getFileName() )) {
-            return false;
-        } else {
-            $this->fileContent = htmlentities(file_get_contents($this->getFileName()));            
-            if (preg_match("/$this->email/", $this->fileContent)) {
-                $this->error = true;
-                return true;
-            } else {
-                $this->error = false;
-                return false;
-            }
-             
-        }
-    }
-
-    private function getFileName() {
-        date_default_timezone_set('Europe/Kiev');
-        $this->curDate = date("d_m_Y");
-        $this->filename = "registration_".$this->curDate.".txt";
-        return $this->filename;
-    }
-
-
-    public function addUser() {        
-        $this->fp = fopen($this->getFileName(), "a") or die("can't open file");
-        fwrite($this->fp, 'Имя: '.$this->fname.', Фамилия: '. $this->lname. ', email: '.$this->email.', Тип билета: '.$this->type."\r\n");
-        fclose($this->fp);        
-        $_SESSION['auth']['user'] = $this->fname;
-        return true;
-    }  
-    
+	public function errorMessages($message) {
+		array_push($this->errorAr, $message);
+		return $this->errorAr;
+	}
+	
 }
 
+$result = [];
+$rawPost = json_decode(file_get_contents("php://input"));
+mb_parse_str($rawPost, $result);
 
-if (empty($_POST["fname"])) {    
-    $checkEmail = new UserRegister(null, null, $_POST["email"], null);
-    if ($checkEmail->checkUniq()) {
-        echo 'error';
-    } else {
-        echo 'success';
-    }
+if (!empty($_POST["live"])) {
+	$checkEmail = new UserRegister(null, null, $_POST["email"], null);
+	$checkEmail->checkUniq();
+	
+	if ( count($checkEmail->errorAr) ) {
+		echo "error";
+	} 
 
 } else {
-    $newUser = new UserRegister($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["type"]);
+	$newUser = new UserRegister($result["fname"], $result["lname"], $result["email"], $result["ticket"]);
+	$newUser->checkData();
+	$newUser->checkFullName();
+	$newUser->checkEmail();
+	$newUser->checkUniq();
 
-
-if ($newUser->checkData()) {
-    echo 'All fields must be filled <br>';    
-}
-
-if ($newUser->checkEmail()) {
-    echo 'Email is invalid <br>';    
-}
-
-if ($newUser->checkNameLength()) {
-    echo 'Username is too short <br>';    
-}
-
-if ($newUser->checkUniq()) {
-    echo 'Email already exist, try another';
-} 
-
-if (!$newUser->error) {
-    $newUser->addUser();
-    echo 'success';
-}
-
+	if ( count($newUser->errorAr) ) {
+		foreach($newUser->errorAr as $key=>$value) {
+			echo $value."<br>";
+		}
+	} else {
+		echo $newUser->addUser();
+	}
 }
