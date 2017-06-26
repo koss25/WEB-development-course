fcb939ddeb032a09d073465c1753cbdfe0fce734
Konstantin <skpalad@gmail.com>
2017-06-27 00:31:03 +0300

    wp first commit

---
+++ wp-content/plugins/remove-wp-overhead/includes/class-remove-wordpress-overhead.php
@@ -0,0 +1,468 @@
+<?php
+namespace Remove_Wordpress_Overhead;
+use Remove_Wordpress_Overhead\Remove_Wordpress_Overhead;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Remove_Wordpress_Overhead {
+
+	/**
+	 * The single instance of Remove_Wordpress_Overhead.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Settings class object
+	 * @var     object
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $settings = null;
+
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_version;
+
+	/**
+	 * The base prefix.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_base;
+
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_token;
+
+	/**
+	 * The main plugin file.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $file;
+
+	/**
+	 * The main plugin directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $dir;
+
+	/**
+	 * The plugin assets directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_dir;
+
+	/**
+	 * The plugin assets URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_url;
+
+	/**
+	 * Suffix for Javascripts.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $script_suffix;
+
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function __construct ( $file = '', $version = '1.0.0', $base = 'wp_plugin_' ) {
+		$this->_version = $version;
+		$this->_base = $base;
+		$this->_token = 'remove_wordpress_overhead';
+
+		// Load plugin environment variables
+		$this->file = $file;
+		$this->dir = dirname( $this->file );
+		$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
+		$this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+
+		$this->script_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		register_activation_hook( $this->file, array( $this, 'install' ) );
+
+		// Load admin JS & CSS
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ), 10, 1 );
+
+		// Load API for generic admin functions
+		if ( is_admin() ) {
+			$this->admin = new Remove_Wordpress_Overhead_Admin_API();
+		}
+
+		// Handle localisation
+		$this->load_plugin_textdomain();
+		add_action( 'init', array( $this, 'load_localisation' ), 0 );
+
+		// do the actual removing of stuff
+		$this->removeStuff();
+
+		// delete transients on save options page
+		add_action( 'load-settings_page_remove_wordpress_overhead_settings', array( $this, 'deleteTransients' ) );
+
+	} // End __construct ()
+
+	/**
+	 * Wrapper function to register a new post type
+	 * @param  string $post_type   Post type name
+	 * @param  string $plural      Post type item plural name
+	 * @param  string $single      Post type item single name
+	 * @param  string $description Description of post type
+	 * @return object              Post type class object
+	 */
+	public function register_post_type ( $post_type = '', $plural = '', $single = '', $description = '', $options = array() ) {
+
+		if ( ! $post_type || ! $plural || ! $single ) return;
+
+		$post_type = new Remove_Wordpress_Overhead_Post_Type( $post_type, $plural, $single, $description, $options );
+
+		return $post_type;
+	}
+
+	/**
+	 * Wrapper function to register a new taxonomy
+	 * @param  string $taxonomy   Taxonomy name
+	 * @param  string $plural     Taxonomy single name
+	 * @param  string $single     Taxonomy plural name
+	 * @param  array  $post_types Post types to which this taxonomy applies
+	 * @return object             Taxonomy class object
+	 */
+	public function register_taxonomy ( $taxonomy = '', $plural = '', $single = '', $post_types = array(), $taxonomy_args = array() ) {
+
+		if ( ! $taxonomy || ! $plural || ! $single ) return;
+
+		$taxonomy = new Remove_Wordpress_Overhead_Taxonomy( $taxonomy, $plural, $single, $post_types, $taxonomy_args );
+
+		return $taxonomy;
+	}
+
+	/**
+	 * Load admin CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_styles ( $hook = '' ) {
+		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin' . $this->script_suffix . '.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-admin' );
+	} // End admin_enqueue_styles ()
+
+	/**
+	 * Load admin Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_scripts ( $hook = '' ) {
+		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-admin' );
+	} // End admin_enqueue_scripts ()
+
+	/**
+	 * Load plugin localisation
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_localisation () {
+		load_plugin_textdomain( 'remove-wordpress-overhead', false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_localisation ()
+
+	/**
+	 * Load plugin textdomain
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_plugin_textdomain () {
+	    $domain = 'remove-wordpress-overhead';
+
+	    $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+	    load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
+	    load_plugin_textdomain( $domain, false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_plugin_textdomain ()
+
+	/**
+	 * Main Remove_Wordpress_Overhead Instance
+	 *
+	 * Ensures only one instance of Remove_Wordpress_Overhead is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see Remove_Wordpress_Overhead()
+	 * @return Main Remove_Wordpress_Overhead instance
+	 */
+	public static function instance ( $file = '', $version = '1.0.0', $base = 'wp_plugin_' ) {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self( $file, $version, $base );
+		}
+		return self::$_instance;
+	} // End instance ()
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __clone ()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __wakeup ()
+
+	/**
+	 * Installation. Runs on activation.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function install () {
+		$this->_log_version_number();
+	} // End install ()
+
+	/**
+	 * Log the plugin version number.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	private function _log_version_number () {
+		update_option( $this->_token . '_version', $this->_version );
+	} // End _log_version_number ()
+
+	/**
+	 * Do the removing of stuff
+	 * @access    private
+	 * @since     1.0.0
+	 * @return    void
+	 */
+	private function removeStuff() {
+		$options = array();
+		// get transient with options or set it of not available
+		if ( false === $options = get_transient( $this->_base . 'transient_settings' ) ) {
+			$options['rsd_link'] = get_option( $this->_base . 'remove_rsd_link' );
+			$options['wlwmanifest'] = get_option( $this->_base . 'remove_wlwmanifest_link' );
+			$options['feed_links'] = get_option( $this->_base . 'remove_rss_feed_links' );
+			$options['next_prev'] = get_option( $this->_base . 'remove_next_prev_links' );
+			$options['shortlink'] = get_option( $this->_base . 'remove_shortlink' );
+			$options['wp_generator'] = get_option( $this->_base . 'remove_wp_generator' );
+			$options['ver'] = get_option( $this->_base . 'remove_version_numbers_from_style_script' );
+			$options['emojicons'] = get_option( $this->_base . 'disable_wp_emojicons' );
+			$options['json_api'] = get_option( $this->_base . 'disable_json_api' );
+			$options['canonical'] = get_option( $this->_base . 'remove_canonical' );
+			$options['woo_generator'] = get_option( $this->_base . 'remove_woo_generator' );
+			$options['widgets'] = get_option( $this->_base . 'disable_wp_widgets' );
+			set_transient( $this->_base . 'transient_settings', $options );
+		}
+
+		// remove really simple discovery link
+		if ( isset( $options['rsd_link'] ) && 'on' == $options['rsd_link'] ) {
+			remove_action('wp_head', 'rsd_link');
+		}
+
+		// remove wlwmanifest.xml (needed to support windows live writer)
+		if ( isset( $options['wlwmanifest'] ) && 'on' == $options['wlwmanifest'] ) {
+			remove_action('wp_head', 'wlwmanifest_link'); 
+		}
+
+		// remove rss feed and exta feed links (make sure you add them in yourself if you are using as RSS service
+		if ( isset( $options['feed_links'] ) && 'on' == $options['feed_links'] ) {
+			remove_action('wp_head', 'feed_links', 2);
+			remove_action('wp_head', 'feed_links_extra', 3);
+		}
+
+		// remove the next and previous post links
+		if ( isset( $options['next_prev'] ) && 'on' == $options['next_prev'] ) {
+			remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+			remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+		}
+
+		// remove the shortlink url from header
+		if ( isset( $options['shortlink'] ) && 'on' == $options['shortlink'] ) {
+			remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+		}
+
+		// remove wordpress generator version
+		if ( isset( $options['wp_generator'] ) && 'on' == $options['wp_generator'] ) {
+			add_filter( 'the_generator', array( $this, 'wp_remove_version' ) );
+		}
+
+		// remove ver= after style and script links
+		if ( isset( $options['ver'] ) && 'on' == $options['ver'] ) {
+			add_filter( 'style_loader_src', array( $this, 'remove_ver_css_js' ), 9999 );
+			add_filter( 'script_loader_src', array( $this, 'remove_ver_css_js' ), 9999 );
+		}
+
+		// remove emoji styles and script from header
+		if ( isset( $options['emojicons'] ) && 'on' == $options['emojicons'] ) {
+			add_action( 'init', array( $this, 'disable_wp_emojicons' ) );
+		}
+
+		// disable json api and remove link from header
+		if ( isset( $options['json_api'] ) && 'on' == $options['json_api'] ) {
+			add_action( 'after_setup_theme', array( $this, 'remove_json_api' ) );
+			add_action( 'after_setup_theme', array( $this, 'disable_json_api' ) );
+		}
+
+		// remove canonical link
+		if ( isset( $options['canonical'] ) && 'on' == $options['canonical'] ) {
+			remove_action('embed_head', 'rel_canonical');
+			add_filter( 'wpseo_canonical', '__return_false' );
+		}
+
+		// remove woocommerce generator version
+		if ( isset( $options['woo_generator'] ) && 'on' == $options['woo_generator'] ) {
+			remove_action( 'wp_head','wc_generator_tag' );
+		}
+
+		// disable wp widgets
+		if ( isset( $options['widgets'] ) && '' != $options['widgets'] && is_array( $options['widgets'] ) ) {
+			// unregister widgets
+			add_action('widgets_init', array( $this, 'unregister_default_widgets' ), 11);
+		}
+	}
+
+	/**
+	 * Remove JSON API links from header
+	 * @access    public
+	 * @since     1.0.0
+	 * @return    void
+	 */
+	public function remove_json_api () {
+		remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+		remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+		remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+		add_filter( 'embed_oembed_discover', '__return_false' );
+		remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+		remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+	}
+
+	/**
+	 * Disable JSON API
+	 * @access    public
+	 * @since     1.0.0
+	 * @return    void
+	 */
+	public function disable_json_api () {
+		add_filter('json_enabled', '__return_false');
+		add_filter('json_jsonp_enabled', '__return_false');
+		add_filter('rest_enabled', '__return_false');
+		add_filter('rest_jsonp_enabled', '__return_false');
+	}
+
+	/**
+	 * Unregister WP Widgets
+	 * @access    public
+	 * @since     1.0.0
+	 * @return    void
+	 */
+	public function unregister_default_widgets() {
+		$options['widgets'] = get_option( $this->_base . 'disable_wp_widgets' );
+		foreach( $options['widgets'] as $widget ) {
+			unregister_widget( $widget );
+		}
+	}
+
+	/**
+	 * Delete settings transient on save options page
+	 * @access    public
+	 * @since     1.0.0
+	 * @return    void
+	 */
+	public function deleteTransients() {
+		if( isset( $_GET['settings-updated'] ) && $_GET['settings-updated'] ) {
+			delete_transient( $this->_base . 'transient_settings' );
+		}
+	}
+
+	/**
+	 * Remove WP generator link
+	 * @access    public
+	 * @since     1.1.0
+	 * @return    void
+	 */
+	public function wp_remove_version() {
+		return '';
+	}
+
+	/**
+	 * Remove version numbers at the end of css and js files
+	 * @access    public
+	 * @since     1.1.0
+	 * @return    void
+	 */
+	public function remove_ver_css_js( $src ) {
+		if ( strpos( $src, 'ver=' ) ) {
+			$src = remove_query_arg( 'ver', $src );
+		}
+		return $src;
+	}
+
+	/**
+	 * Disable WP emojicons
+	 * @access    public
+	 * @since     1.1.0
+	 * @return    void
+	 */
+	public function disable_wp_emojicons() {
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		add_filter( 'tiny_mce_plugins', array( $this, 'disable_emojicons_tinymce' ) );
+	}
+
+	/**
+	 * Disable WP emojicons from TinyMCE
+	 * @access    public
+	 * @since     1.1.0
+	 * @return    void
+	 */
+	public function disable_emojicons_tinymce( $plugins ) {
+		if ( is_array( $plugins ) ) {
+			return array_diff( $plugins, array( 'wpemoji' ) );
+		} else {
+			return array();
+		}
+	}
+
+}
---
+++ wp-content/plugins/remove-wp-overhead/readme.txt
@@ -0,0 +1,81 @@
+=== Remove Wordpress Overhead ===
+Contributors: Omnisite
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8ZS4SEBD4EA7W
+Tags: wordpress, header, remove widgets, disable widgets, remove, clean, strip, version, disable, rsd link, wp generator, feed link, rss feed, shortlink, next, prev, wp generator, version number, wlwmanifest, emojicons, json api
+Requires at least: 3.9
+Tested up to: 4.6
+Stable tag: 1.1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Remove overhead from the <head> HTML, speed up your website and disable widgets you don't use
+
+== Description ==
+
+A standard WP installation contains many links in the head of your HTML (which slow down your site) and has standard widgets you might never use. You can now install this plugin and check the items you want to have removed. The saved options are cached for better performance.
+
+Header items you can remove:
+* Remove RSD / EditURI Link
+* Remove WLW Manifest Link
+* Remove RSS Feed Links
+* Remove Next & Prev Post Links
+* Remove Shortlink URL
+* Remove WP Generator Meta
+* Remove Version Numbers from Style and Script Links
+* Disable WP Emoji / emoticons
+* Disable JSON API
+* Disable Canonical URL
+* Remove WooCommerce Generator Meta
+
+You can disable the following widgets:
+* Archives
+* Calendar
+* Categories
+* Links
+* Meta
+* Nav Menu
+* Pages
+* Recent Comments
+* Recent Posts
+* RSS
+* Search
+* Tag Cloud
+* Text
+
+== Installation ==
+
+Installing "Remove Wordpress Overhead" can be done either by searching for "Remove Wordpress Overhead" via the "Plugins > Add New" screen in your WordPress dashboard, or by using the following steps:
+
+1. Download the plugin via WordPress.org
+1. Upload the ZIP file through the 'Plugins > Add New > Upload' screen in your WordPress dashboard
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. screenshot-1: settings screen
+
+== Changelog ==
+
+= 1.1.0 =
+* 2016-10-23
+* ability to remove canonical url from header
+* ability to remove WooCommerce generator tag from header
+* add plugin git URL
+* add "select all" op settings page
+* namespace classes to avoid function name collisions
+* add css sliders and checkboxes on settings page
+* added info buttons on the settings page
+
+= 1.0.2 =
+* 2016-09-23
+* Correct author name in php file
+* Add donate link in readme
+* Better readme text
+
+= 1.0.1 =
+* 2016-09-18
+* Add license in composer.json
+
+= 1.0.0 =
+* 2016-09-18
+* Initial release
